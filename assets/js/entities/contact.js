a907555b88887b5445822ea2e182fb1cad0458dd
Rob hammann <runrobrun@gmail.com>
2014-12-01 11:08:51 -0600

    refactored code to use a request to get data from entity

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -6,6 +6,46 @@ ContactManager.module("Entities", function(Entities, ContactManger, Backbone, Ma
     model: Entities.Contact,
     comparator: "firstName"
   });
+
+  var contacts;
+
+  var initializeContacts = function() {
+    contacts = new ContactManager.Entities.ContactCollection([
+
+      {
+        id: 1,
+        firstName: 'Bob',
+        lastName: 'Brigham',
+        phoneNumber: '555-0163'
+      },
+      {
+        id: 2,
+        firstName: 'Alice',
+        lastName: 'Arten',
+        phoneNumber: '555-0184'
+      },
+      {
+        id: 3,
+        firstName: 'Charlie',
+        lastName: 'Campbell',
+        phoneNumber: '555-1029'
+      }
+    ]);
+  };
+
+  var API = {
+    getContactEntities: function() {
+      if (contacts === undefined) {
+        initializeContacts();
+      }
+      return contacts;
+    }
+  };
+
+  ContactManger.reqres.setHandler("contact:entities", function() {
+    return API.getContactEntities();
+  })
+
 });
 
 
