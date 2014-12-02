1c222ba4fbfb555f19a18ae5fc96b7a050c67b91
Rob hammann <runrobrun@gmail.com>
2014-12-02 08:13:44 -0600

    added click event to tr

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,7 +1,16 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: 'tr',
-    template: "#contact-list-item"
+    template: "#contact-list-item",
+
+    events: {
+      "click": "highlightName"
+    },
+
+    highlightName: function() {
+      this.$el.toggleClass("warning");
+    }
+
   });
 
   List.Contacts = Marionette.CompositeView.extend({
