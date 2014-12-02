4e972fe934f8708590a30a0b95f05437716573d0
Rob hammann <runrobrun@gmail.com>
2014-12-02 08:08:13 -0600

    made new composite view

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -1,15 +1,15 @@
 ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
-    tagName: 'tr',
+    tagName: 'li',
     template: "#contact-list-item"
   });
 
   List.Contacts = Marionette.CompositeView.extend({
-    tagName: 'table',
+    tagName: 'div',
     className: 'table table-hover',
     template: "#contact-list",
     childView: List.Contact,
-    childViewContainer: "tbody"
+    childViewContainer: "ul"
   });
 
 });
