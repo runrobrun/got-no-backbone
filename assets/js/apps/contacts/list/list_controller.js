e4c660c56d9a747274d2fe0818e963070749cd8d
Rob hammann <runrobrun@gmail.com>
2014-12-04 09:08:09 -0600

    adding routes to contact and refactoring code to put route control in better location.

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -12,7 +12,7 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager, Backbon
       });
 
       contactsListView.on("childview:contact:show", function(childView, model) {
-        ContactManager.ContactsApp.Show.Controller.showContact(model);
+        ContactManager.trigger("contact:show",model.get("id"));
       });
 
       ContactManager.mainRegion.show(contactsListView);
