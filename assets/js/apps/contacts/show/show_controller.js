edc2e2d5707e3460196f9670b4c82de5d45d1b7e
Rob hammann <runrobrun@gmail.com>
2014-12-12 13:53:45 -0600

    fixed routes for show single contact

--- assets/js/apps/contacts/show/show_controller.js
+++ assets/js/apps/contacts/show/show_controller.js
@@ -3,9 +3,14 @@ ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbon
     showContact: function(id) {
       var contacts = ContactManager.request("contact:entities");
       var model = contacts.get(id);
-      var contactView = new Show.Contact({
-        model: model
-      });
+      var contactView;
+      if(model !== undefined) {
+        var contactView = new Show.Contact({
+          model: model
+        });
+      } else {
+        contactView = new Show.MissingContact();
+      }
 
       ContactManager.mainRegion.show(contactView);
     }
